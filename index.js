64603261a6ad0bd9f0e58e830a6d28b29a0a44a7
Nagib Tharani <ntharani@gmail.com>
2017-08-13 21:42:07 -0700

    interim commit

--- index.js
+++ index.js
@@ -14,20 +14,50 @@ console.log("Le response mon ami, ", viewmodel.nnn);
 
 
 // Overall viewmodel for this screen, along with initial state
-function ReservationsViewModel() {
+function PlaceObjectViewModel() {
     var self = this;
+        
+    let xx =  
+        [
+            {name: "Hubbub Sandwiches", lat: 49.282078, lng: -123.12287, checkins: 3508},
+            {name: "Japadog", lat: 49.28318583609762, lng: -123.12276455990715, checkins: 2694},
+            {name: "Tacofino Cantina", lat: 49.282394974979276, lng: -123.12094607150951, checkins: 502},
+            {name: "Italian Kitchen", lat: 49.284456, lng: -123.122105, checkins: 4247},
+            {name: "Mom's Grilled Cheese Truck", lat: 49.28224303303845, lng: -123.1207455787171, checkins: 1163},
+            {name: "Shuraku Sake Bar and Bistro", lat: 49.28102573386839, lng: -123.12047300498325, checkins: 2277},
+            {name: "JOEY Burrard", lat: 49.28288439342585, lng: -123.12340983872221, checkins: 5671},
+            {name: "Vancouver Art Gallery Cafe", lat: 49.28261607531749, lng: -123.12090396881104, checkins: 2164}
+        ];
+        
+        console.log("Typeof xx is: ",typeof(xx));
+
 
     // Editable data
-    self.seats = ko.observableArray([
-    ]);
+    self.place = ko.observableArray(xx
+        // [
+        //     {name: "Hubbub Sandwiches", lat: 49.282078, lng: -123.12287, checkins: 3508},
+        //     {name: "Japadog", lat: 49.28318583609762, lng: -123.12276455990715, checkins: 2694},
+        //     {name: "Tacofino Cantina", lat: 49.282394974979276, lng: -123.12094607150951, checkins: 502},
+        //     {name: "Italian Kitchen", lat: 49.284456, lng: -123.122105, checkins: 4247},
+        //     {name: "Mom's Grilled Cheese Truck", lat: 49.28224303303845, lng: -123.1207455787171, checkins: 1163},
+        //     {name: "Shuraku Sake Bar and Bistro", lat: 49.28102573386839, lng: -123.12047300498325, checkins: 2277},
+        //     {name: "JOEY Burrard", lat: 49.28288439342585, lng: -123.12340983872221, checkins: 5671},
+        //     {name: "Vancouver Art Gallery Cafe", lat: 49.28261607531749, lng: -123.12090396881104, checkins: 2164}
+        // ]        
+    );
 
-    (viewmodel.nnn).forEach((obj) => (self.seats).push(obj));
+    // let interim = [];
+    // (viewmodel.nnn).forEach((obj) => interim.push(obj) )
+    // console.log("interim is, ",interim );
+    // self.place(viewmodel.nnn);
+
+    // Is there a reason this code below doesn't work?
+    // (viewmodel.nnn).forEach((obj) => (self.place).push(obj));
     console.log("vm is,", viewmodel.nnn);
-    console.log("self.seats is,", self.seats());
-    
+    console.log("self.place is,", self.place());    
 }
 
-ko.applyBindings(new ReservationsViewModel());
+ko.applyBindings(new PlaceObjectViewModel());
 
         // {name: "Hubbub Sandwiches", lat: 49.282078, lng: -123.12287, checkins: 3508},
         // {name: "Japadog", lat: 49.28318583609762, lng: -123.12276455990715, checkins: 2694},
@@ -37,3 +67,4 @@ ko.applyBindings(new ReservationsViewModel());
         // {name: "Shuraku Sake Bar and Bistro", lat: 49.28102573386839, lng: -123.12047300498325, checkins: 2277},
         // {name: "JOEY Burrard", lat: 49.28288439342585, lng: -123.12340983872221, checkins: 5671},
         // {name: "Vancouver Art Gallery Cafe", lat: 49.28261607531749, lng: -123.12090396881104, checkins: 2164}
+
