c2beb853a71a0a88f8725516d40ce6095be8e29a
Nagib Tharani <ntharani@gmail.com>
2017-08-13 20:04:38 -0700

    interim commit, knockout integrated, works with manual objects. push to observable array borked

--- index.js
+++ index.js
@@ -1,68 +1,39 @@
-import 'whatwg-fetch'
-import Promise from 'promise-polyfill'; 
-
-// To add to window
-if (!window.Promise) {
-  window.Promise = Promise;
-}
-
-const FOURSQ_CLIENT_ID = "W40TGAOG1505I3W4FJGKJIOCXWU22EG33OVA0M3LKI3G45HO";
-const FOURSQ_CLIENT_SECRET = "4LHO1EKA2ENAWWAX2E31BWUQG2I5GCSJKOSFJJCT4Y2XGZBL";
-
-// var googleMapsClient = require('@google/maps').createClient({
-//   key: 'AIzaSyDWAtCG4fzvlkJQpUGxeT-JndYmZCaKsvU'
-// })
-
-const getVenues = "https://api.foursquare.com/v2/venues/explore?v=20131016&ll=49.2827%2C-123.1207&section=food&novelty=new&client_id=";
-let ans = getVenues+FOURSQ_CLIENT_ID + "&client_secret=" + FOURSQ_CLIENT_SECRET;
-console.log(ans)
-function checkStatus(response) {
-  if (response.status >= 200 && response.status < 300) {
-    return response
-  } else {
-    var error = new Error(response.statusText)
-    error.response = response
-    throw error
-  }
-}
-
-function parseJSON(response) {
-  return response.json()
-}
-
-function getfsdata() {
-console.log("I'm a transactin here..")
-fetch(ans)
-  .then(checkStatus)
-  .then(parseJSON)
-  .then(function(data) {
-    console.log('request succeeded with JSON response', data)
-    const choices = data.response.groups[0].items;
-    const ans2 = choices.map(function(obj){
-        return { 
-            name: obj.venue.name,
-            lat: obj.venue.location.lat,
-            lng: obj.venue.location.lng,
-            checkins: obj.venue.stats.checkinsCount
-        }
-    })
-    console.log("choices is,", ans2);
-  }).catch(function(error) {
-    console.log('request failed', error)
-  })
-}
-
-getfsdata()
-
-// Get clear on namespacing. 
+import * as viewmodel from "./viewmodel.js";
 
 var map;
 window.initMap = function() {
-console.log("Calling const from index.js, ", FOURSQ_CLIENT_ID );
-
-map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 49.2827, lng: -123.1207},
-    zoom: 15
-});
-console.log(map);
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 49.2827, lng: -123.1207},
+        zoom: 15
+    });
+    console.log(map);
 }
+
+viewmodel.getfsdata();
+console.log("Le response mon ami, ", viewmodel.nnn);
+
+
+// Overall viewmodel for this screen, along with initial state
+function ReservationsViewModel() {
+    var self = this;
+
+    // Editable data
+    self.seats = ko.observableArray([
+    ]);
+
+    (viewmodel.nnn).forEach((obj) => (self.seats).push(obj));
+    console.log("vm is,", viewmodel.nnn);
+    console.log("self.seats is,", self.seats());
+    
+}
+
+ko.applyBindings(new ReservationsViewModel());
+
+        // {name: "Hubbub Sandwiches", lat: 49.282078, lng: -123.12287, checkins: 3508},
+        // {name: "Japadog", lat: 49.28318583609762, lng: -123.12276455990715, checkins: 2694},
+        // {name: "Tacofino Cantina", lat: 49.282394974979276, lng: -123.12094607150951, checkins: 502},
+        // {name: "Italian Kitchen", lat: 49.284456, lng: -123.122105, checkins: 4247},
+        // {name: "Mom's Grilled Cheese Truck", lat: 49.28224303303845, lng: -123.1207455787171, checkins: 1163},
+        // {name: "Shuraku Sake Bar and Bistro", lat: 49.28102573386839, lng: -123.12047300498325, checkins: 2277},
+        // {name: "JOEY Burrard", lat: 49.28288439342585, lng: -123.12340983872221, checkins: 5671},
+        // {name: "Vancouver Art Gallery Cafe", lat: 49.28261607531749, lng: -123.12090396881104, checkins: 2164}
---
+++ viewmodel.js
@@ -0,0 +1,62 @@
+import 'whatwg-fetch'
+import Promise from 'promise-polyfill'; 
+
+// To add to window
+if (!window.Promise) {
+  window.Promise = Promise;
+}
+
+const FOURSQ_CLIENT_ID = "W40TGAOG1505I3W4FJGKJIOCXWU22EG33OVA0M3LKI3G45HO";
+const FOURSQ_CLIENT_SECRET = "4LHO1EKA2ENAWWAX2E31BWUQG2I5GCSJKOSFJJCT4Y2XGZBL";
+
+// var googleMapsClient = require('@google/maps').createClient({
+//   key: 'AIzaSyDWAtCG4fzvlkJQpUGxeT-JndYmZCaKsvU'
+// })
+
+const getVenues = "https://api.foursquare.com/v2/venues/explore?v=20131016&ll=49.2827%2C-123.1207&section=food&novelty=new&client_id=";
+let ans = getVenues+FOURSQ_CLIENT_ID + "&client_secret=" + FOURSQ_CLIENT_SECRET;
+// console.log(ans)
+
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response
+  } else {
+    var error = new Error(response.statusText)
+    error.response = response
+    throw error
+  }
+}
+
+function parseJSON(response) {
+  return response.json()
+}
+
+let nnn = [];
+
+module.exports = {
+
+nnn, // Thank you ES6.
+getfsdata: function () {
+console.log("I'm a transactin here..")
+fetch(ans)
+  .then(checkStatus)
+  .then(parseJSON)
+  .then(function(data) {
+    console.log('request succeeded with JSON response', data)
+    const choices = data.response.groups[0].items;
+    const ans2 = choices.map(function(obj){
+        let resp = { 
+            name: obj.venue.name,
+            lat: obj.venue.location.lat,
+            lng: obj.venue.location.lng,
+            checkins: obj.venue.stats.checkinsCount
+        }
+        nnn.push(resp);
+        return resp;
+    })
+    console.log("choices is,", ans2);
+  }).catch(function(error) {
+    console.log('request failed', error)
+  })
+},
+};
