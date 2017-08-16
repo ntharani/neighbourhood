1639169eb8df42f45af2f6820f714831061d99a1
Nagib Tharani <ntharani@gmail.com>
2017-08-16 07:26:48 -0700

    Refactored code to leverage ES2015 class syntactic sugar

--- viewmodel.js
+++ viewmodel.js
@@ -38,96 +38,113 @@ function getfsdata(callback) {
       console.log('request succeeded with JSON response', data)
       const choices = data.response.groups[0].items;
       const ans2 = choices.map(function(obj){
-          let resp = { 
-              name: obj.venue.name,
-              lat: obj.venue.location.lat,
-              lng: obj.venue.location.lng,
-              checkins: obj.venue.stats.checkinsCount
-          }
-          return resp;
+        let resp = { 
+            name: obj.venue.name,
+            lat: obj.venue.location.lat,
+            lng: obj.venue.location.lng,
+            checkins: obj.venue.stats.checkinsCount
+        }
+        return resp;
       })
       console.log("choices is,", ans2);
       callback(ans2)
     }).catch(function(error) {
-      console.log('request failed', error)
-    })
+    console.log('request failed', error)
+  })
 }
 
 /*
 Creates a Google Maps Marker object with data pulled in from
 PlaceListViewModel
 */
-function PlaceModel(place){
-  let self = this;
-  self.name = place.name;
-  self.lat = place.lat;
-  self.lng = place.lng;
-  self.checkins = place.checkins;
+class PlaceModel{
 
-  // Create the map marker for this SubwayStation object
-  self.mapMarker = new google.maps.Marker({
-      position: {lat: self.lat, lng: self.lng},
-      map: map,
-      title: self.name
-  });
+  constructor(place) {
+    this.name = place.name;
+    this.lat = place.lat;
+    this.lng = place.lng;
+    this.checkins = place.checkins;
 
-  // Create the info window for this  object
-  self.infoWindow = new google.maps.InfoWindow();
+    // Create the map marker for this SubwayStation object
+    this.mapMarker = new google.maps.Marker({
+        position: {lat: this.lat, lng: this.lng},
+        map: map,
+        title: this.name
+    });
 
-  self.mapMarker.setAnimation(google.maps.Animation.BOUNCE);
-
-self.infoWindow.open(map, self.mapMarker);
-
-  self.mapMarker.setAnimation(null);
-  self.infoWindow.close();
+    // Create the info window for this  object
+    this.infoWindow = new google.maps.InfoWindow();
 
 
-  /*
-  Don't want all of them to open by default, just
-  when one is clicked. Therefore query and close any other peer 
-  PlaceModel objects
-  */
-  // self.infoWindow.open(map, self.mapMarker);
+    this.showInfoWindow = () => {
+        // Build the basic info window content, if hasn't been done
+        if (!this.infoWindow.getContent()) {
+            // Initialize basic info window content and display it
+            this.infoWindow.setContent('Loading content...');
+            let content = '<h3>' + this.name + '</h3>';
+            content += '<p>Check-ins: ' + this.checkins + '</p>';
+            this.infoWindow.setContent(content);
+        }
 
+        // Show info window
+        this.infoWindow.open(map, this.mapMarker);
+    };
+
+    this.showInfoWindow();
+
+
+    this.mapMarker.setAnimation(google.maps.Animation.BOUNCE);
+
+    // this.infoWindow.open(map, this.mapMarker);
+
+    // this.mapMarker.setAnimation(null);
+    // this.infoWindow.close();
+
+
+    /*
+    Don't want all of them to open by default, just
+    when one is clicked. Therefore query and close any other peer 
+    PlaceModel objects
+    */
+    // self.infoWindow.open(map, self.mapMarker);
+  }
 }
 
 
 /*
 1. Uses Fetch to pull in a list of venues from Foursquare.
-2. Assigns the array result to self.place in Knockout observable array.
-3. Sets up individual PlaceModel
-4. Has methods to filter and show specific place(s)
+2. Assigns the array result to this.placeList in Knockout observable array.
+3. Sets up individual PlaceModel with methods to filter & show specific place(s)
 */
-function PlaceListViewModel() {
-    let self = this;
-        
-    // By default this will contain everything fetched from JSON
-    self.place = ko.observableArray([]);
-
-    self.filter = ko.observable('');
-    self.isVisible = ko.observable(true);
-
-
+class PlaceViewModel {        
+  constructor(){
+    this.placeList = ko.observableArray([]);
+    this.filter = ko.observable('');
+    this.isVisible = ko.observable(true);
 
     // Use Filter Here - Knockout computed observable
     // KeyUp binding equivalent is via Knockout
 
-    getfsdata(function(result){
-      console.log("result is, ",result);
-      // Here we set the push this FourSquare venue list to the observable array.
-      self.place(result);
-      // Simultaneously, we instantiate a PlaceModel object from our PlaceListViewModel
-      // Each place will be able to set its own Google Map properties.
+    getfsdata((result) => {
+      console.log("result is, ", result);
+      // Grab the FourSquare venue list and push to the placeList observable array.
+      // Simultaneously, we instantiate a PlaceModel object
+      // Each placeModel will be able to set its own Google Map properties.
       result.forEach((obj) => {
-        new PlaceModel(obj);
+        this.placeList.push( new PlaceModel(obj) );
       })
     });
-    
+  }
+
+  hello(){
+    console.log("Hola!");
+  }
 }
 
 // ko.applyBindings(new PlaceListViewModel());
-var vm = new PlaceListViewModel();
+var vm = new PlaceViewModel();
 ko.applyBindings(vm);
+// vm.hello();
 
 let map;
 
@@ -138,7 +155,7 @@ let map;
 window.initMap = function() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 49.2827, lng: -123.1207},
-        zoom: 15
+        zoom: 16
     });
     console.log(map);
 }
